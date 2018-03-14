5288cb6c564b0948418fb06084ce0e34e9ec822c
thib-s <thibaut.boissin@gmail.com>
2018-03-14 16:07:49 +0100

    fix bug in screen names

--- scripts/local_execution/run_client.sh
+++ scripts/local_execution/run_client.sh
@@ -2,5 +2,5 @@ cd nosave/env_parallel
 source bin/activate
 for i in `cat /proc/cpuinfo | awk '/^processor/{print $3}'`;
 do
-   screen -dmS engine1 ipengine ~/.ipython/profile_default/security/ipcontroller-engine.json
+   screen -dmS "engine$i" ipengine ~/.ipython/profile_default/security/ipcontroller-engine.json
 done
